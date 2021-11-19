c97f2ccb267c9831dfc93c36d31ef0263ab623df
fffrikkie <93199902+fffrikkie@users.noreply.github.com>
2021-11-19 21:21:15 +0200

    Add files via upload

---
+++ Communications.h
@@ -0,0 +1,13 @@
+#ifndef COMMUNICATIONS_H
+#define COMMUNICATIONS_H
+
+class Communications
+{
+private:
+    /* data */
+public:
+    Communications(/* args */);
+    ~Communications();
+};
+
+#endif
